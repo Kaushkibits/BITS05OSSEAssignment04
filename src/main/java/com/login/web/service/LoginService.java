6047fa22a520775b041aa69fc9f5703d8d5127f8
Kaushki <78991852+Kaushkibits@users.noreply.github.com>
2021-11-01 19:48:04 +0530

    Create LoginService.java
    Service file committed by Kaushki

---
+++ src/main/java/com/login/web/service/LoginService.java
@@ -0,0 +1,13 @@
+package com.login.web.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+	public boolean validateUser(String userid, String password) {
+		return userid.equalsIgnoreCase("Kaushki")
+				&& password.equalsIgnoreCase("qwerty@123");
+	}
+
+}
