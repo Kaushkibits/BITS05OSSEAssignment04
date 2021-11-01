eba0ee0c6b5fcd76ab8db401f4f5c23594c8503b
Kaushki <78991852+Kaushkibits@users.noreply.github.com>
2021-11-01 19:55:11 +0530

    Create OpenSourceSpringBootExampleApplicationTests.java
    final file commit by Kaushki

---
+++ src/test/java/com/login/web/opensourcespringbootexample/OpenSourceSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.web.opensourcespringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSourceSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
